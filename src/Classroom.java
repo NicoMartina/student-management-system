66cbd09953d0495fb568a8735f3c74053818e359
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2025-11-13 14:45:47 -0300

    feat(classroom): created several methods: addStudent(), removeStudent(), listAllStudents(), classAverage() and findTopStudent()

--- src/Classroom.java
+++ src/Classroom.java
@@ -2,55 +2,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Classroom {
+    String className;
+    int id;
+    List<Student> students;
 
-    private String className;
-    private List<Student> students;
 
-    public Classroom(String className) {
-        this.className = className;
-        this.students = new ArrayList<>();
-    }
+   public Classroom(String className, List<Student> students) {
+       this.className = className;
+       this.students = (students != null) ? students : new ArrayList<>();
+   }
 
-    public void addStudent(Student student) {
-        students.add(student);
-        System.out.println("Student: " + student.getName() + " added to " + className);
-    }
+   public void addStudent(Student student){
+       students.add(student);
+       System.out.println("Student " + student.getName() + " was added successfully");
+   }
 
-    public void removeStudent(int id) {
-        students.removeIf(student -> student.getId() == id);
-        System.out.println("Student with ID " + id + "remove from " + className);
-    }
+   public void removeStudent(int id){
+       students.removeIf(student -> student.getId() == id);
+       System.out.println("Student with ID " + id + " has been removed successfully");
+   }
 
-    public void displayAllStudent() {
-        System.out.println("\n=== " + className + " Student ===");
-        for (Student s : students) {
-            s.displayInfo();
-        }
-    }
+   public void listAllStudents() {
+       if (students.isEmpty()) {
+           System.out.println("The classroom is empty.");
+       }
+       for (Student s : students) {
+           s.displayInfo();
+       }
+   }
 
-    public double calculateClassAverage(){
-        if (students.isEmpty()) {
-            System.out.println("No students in" + className);
+   public double classAverage(){
+        if (students.isEmpty()){
+            System.out.println("No students in " + className);
             return 0.0;
         }
-
-        double total = 0.0;
+       double total = 0.0;
         for (Student s : students) {
             total += s.averageGrade();
         }
         return total / students.size();
-    }
+   }
 
-    public Student findTopStudent(){
-        if (students.isEmpty()) return null;
+   public Student findTopStudent(){
+       if (students.isEmpty()){
+           System.out.println("No student in " + className);
+           return null;
+       }
 
-        Student top = students.get(0);
+       Student topStudent = null;
+       double highestAverage = 0.0;
 
-        for (Student s : students) {
-            if (s.averageGrade() > top.averageGrade()) {
-                top = s;
-            }
-        }
-        return top;
-    }
+       for (Student s : students) {
+           double avg = s.averageGrade();
+           if (topStudent == null || avg > highestAverage) {
+               topStudent = s;
+               highestAverage = avg;
+           }
+       }
+
+       System.out.println("TOp student in " + className + ":");
+       topStudent.displayInfo();
+
+
+       return topStudent;
+   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Classroom javaClass = new Classroom("Java Fundamental");
-        Student s1 = new Student("Nico", 1, new ArrayList<>());
+        Classroom classroomPractice = new Classroom("Computer Science 101", null);
+        Student s1 = new Student("Nico", 1, null);
         s1.addGrade(95);
+        s1.addGrade(99);
         s1.addGrade(90);
-        Student s2 = new Student("Maru", 2, new ArrayList<>());
-        s2.addGrade(88);
 
-        javaClass.addStudent(s1);
-        javaClass.addStudent(s2);
+        Student s2 = new Student("Maru", 2, null);
+        s2.addGrade(75);
+        s2.addGrade(70);
+        s2.addGrade(68);
 
-        javaClass.displayAllStudent();
+        Student s3 = new Student("German", 3, null);
+        s3.addGrade(95);
+        s3.addGrade(92);
+        s3.addGrade(97);
 
-        System.out.println("Class average: " + javaClass.calculate);
+        classroomPractice.addStudent(s1);
+        classroomPractice.addStudent(s2);
+        classroomPractice.addStudent(s3);
+
+        classroomPractice.listAllStudents();
+
+        System.out.printf("Class Average: %.2f%n", classroomPractice.classAverage());
+        classroomPractice.findTopStudent();
+
+
     }
 }
